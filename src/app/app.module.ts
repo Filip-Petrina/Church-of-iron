c0f1775c1366286684c990c727e98d03fa582545
filipp <filipp@nivas.hr>
2019-04-22 19:47:24 +0200

    folder structure fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ThemeModule } from './@theme/theme.module';
+
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ThemeModule.forRoot(),
+    NbLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
