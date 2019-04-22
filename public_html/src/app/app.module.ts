d089c249205ed4f88671fc50a4a05a10f0e781d6
filipp <filipp@nivas.hr>
2019-04-22 19:40:28 +0200

    project init, nebular, bootstrap, ionicons, theme module

---
+++ public_html/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ThemeModule } from './@theme/theme.module';
+
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ThemeModule.forRoot(),
+    NbLayoutModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
